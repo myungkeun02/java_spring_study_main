1528f32cc1f34a2c5ed5937a6e55645ab720a584
myungkeun <audrms3568@gmail.com>
2024-03-04 20:22:03 +0900

    git remote

---
+++ blog_study/src/main/java/org/myungkeun/blog_study/BlogStudyApplication.java
@@ -0,0 +1,13 @@
+package org.myungkeun.blog_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogStudyApplication.class, args);
+	}
+
+}
---
+++ blog_study/src/test/java/org/myungkeun/blog_study/BlogStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myungkeun.blog_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
