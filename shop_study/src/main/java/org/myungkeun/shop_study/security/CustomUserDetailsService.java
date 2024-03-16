993efb6cc118bbc62d3f4becd23c2e93096fe0d9
myungkeun <audrms3568@gmail.com>
2024-03-16 20:36:57 +0900

    login error수정

--- shop_study/src/main/java/org/myungkeun/shop_study/security/CustomUserDetailsService.java
+++ shop_study/src/main/java/org/myungkeun/shop_study/security/CustomUserDetailsService.java
@@ -2,10 +2,15 @@ package org.myungkeun.shop_study.security;
 
 import org.myungkeun.shop_study.entity.Member;
 import org.myungkeun.shop_study.repository.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -14,11 +19,11 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.memberRepository = memberRepository;
     }
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email" + email));
-        System.out.println(member.getPassword());
-        return new org.springframework.security.core.userdetails.User(member.getEmail(), member.getPassword(), null);
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        Member member = memberRepository.findByName(name)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with name" + name));
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+        return new org.springframework.security.core.userdetails.User(member.getEmail(), member.getPassword(), Collections.singletonList(authority));
     }
 
 }
